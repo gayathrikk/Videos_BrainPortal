7df272736de431648897c438eb51ac2a9c38bbf5
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-04-21 12:59:59 +0530

    Update MRIPageclicks.java

--- src/test/java/GTask/myyapp/MRIPageclicks.java
+++ src/test/java/GTask/myyapp/MRIPageclicks.java
@@ -32,7 +32,7 @@ public class MRIPageclicks {
 	 @BeforeTest
 	    public void setup() throws MalformedURLException {
 	        DesiredCapabilities dc = DesiredCapabilities.chrome();
-	        URL url = new URL("http://172.20.23.92:4443/wd/hub");
+	        URL url = new URL("http://172.20.23.92:4444/wd/hub");
 	        driver = new RemoteWebDriver(url, dc);
 	    }
 
